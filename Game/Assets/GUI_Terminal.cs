72f5d2550c103390fb14edb1d538c221f2a8c02c
Jonathan Petz <jpeg1994+github@gmail.com>
2014-09-15 10:36:30 -0500

    Updated GUI
    Updated some of the gui functions, and added functionality to the
    overall command line input

--- Game/Assets/GUI_Terminal.cs
+++ Game/Assets/GUI_Terminal.cs
@@ -7,18 +7,32 @@ public class GUI_Terminal : MonoBehaviour {
 	void Start () {
 	
 	}
-	public string input = "Enter a command";
-	public Vector2 scrollPosition;
-	public string consoleLog = "logity log log\nLOGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGASDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDQQQQQQQQQQQQQQWWWWWWWWWWWWWWWWWWWWWW\naASDAs\nAsdasdweqwe";
+	protected string input = "";
+	protected Vector2 scrollPosition;
+	protected string consoleLog = "";
 	void OnGUI () 
 	{
 		//todo... add in a box to contain both the input and the log.
 		// "Submit command" button as well as getting enter to work properly
 		// after submit add to log, if clear command is entered, clear the log
-		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 20), GUILayout.Height (Screen.height- 120));
+		GUILayout.BeginArea(new Rect(10, Screen.height - 123, Screen.width - 20, 123));
+		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 20), GUILayout.Height (98));
 		GUILayout.Label (consoleLog);
 
 		GUILayout.EndScrollView();
-		input = GUI.TextField(new Rect(10, Screen.height- 25, Screen.width - 20,20), input);
+		if(GUI.Button(new Rect(0, 98, 60,20 ),"Submit"))
+		{
+			SubmitCommand();
+		}
+		input = GUI.TextField(new Rect(70,  98, Screen.width - 90,20), input);
+		GUILayout.EndArea();
+
+	}
+	void SubmitCommand()
+	{
+		consoleLog = input + "\n" + consoleLog;
+		consoleLog = "Output From Parser" + "\n" + consoleLog;
+		//terminal output / command execution
+		input = "";
 	}
 }
