e9054569dc5777f142559e89543162a657fdc873
Jonathan Petz <jpegthedev@gmail.com>
2014-09-18 13:11:35 -0500

    Updated GUI
    FINALLY got the “Enter” command to work properly. That particular
    problem took me 2 days to figure out.

--- Game/Assets/GUI_Terminal.cs
+++ Game/Assets/GUI_Terminal.cs
@@ -1,9 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class GUI_Terminal : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 	
 	}
@@ -13,17 +12,25 @@ public class GUI_Terminal : MonoBehaviour {
 	void OnGUI () 
 	{
 
-		// get enter to work properly
-		// after submit add to log, if clear command is entered, clear the log
 		GUILayout.BeginArea(new Rect(10, Screen.height - 123, Screen.width - 20, 123));
 		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 20), GUILayout.Height (98));
-		GUILayout.Label (consoleLog);
 
+		GUILayout.Label (consoleLog);
 		GUILayout.EndScrollView();
+
+		if(GUI.GetNameOfFocusedControl() == "textField")
+		{
+			if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return) 
+			{
+				SubmitCommand();
+			}
+
+		}
 		if(GUI.Button(new Rect(0, 98, 60,20 ),"Submit"))
 		{
 			SubmitCommand();
 		}
+		GUI.SetNextControlName("textField");
 		input = GUI.TextField(new Rect(70,  98, Screen.width - 90,20), input);
 		GUILayout.EndArea();
 
