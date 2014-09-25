b269870e17807e9079abf4f8b0bd516ba0fc13b1
Jonathan Petz <jpegthedev@gmail.com>
2014-09-25 13:07:50 -0500

    Updated the GUI, "One more time"
    Updated several assets and stylized the input and output for the
    command line stuff

--- Game/Assets/Scripts/GUI_Terminal.cs
+++ Game/Assets/Scripts/GUI_Terminal.cs
@@ -10,12 +10,16 @@ public class GUI_Terminal : MonoBehaviour {
 	public Vector2 scrollPosition;
 	protected string consoleLog = "";
 	protected ArrayList commandHistory = new ArrayList();
+	public Texture clb; //Command Line Background
 	public GUISkin skin;
 	void OnGUI () 
 	{
 		GUI.skin = skin;
-		GUILayout.BeginArea(new Rect(10, Screen.height -  (Screen.height * 256/768), Screen.width - 20, (Screen.height * 256/768)));
-		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 20), GUILayout.Height (Screen.height * 211/768));
+		GUI.DrawTexture(new Rect(0,  Screen.height - (Screen.height * 256/768), Screen.width, Screen.height * 256/768), clb, ScaleMode.StretchToFill, true, 10.0F);
+
+
+		GUILayout.BeginArea(new Rect(16, Screen.height -  (Screen.height * 240/768), Screen.width - 28, (Screen.height * 240/768)));
+		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 28), GUILayout.Height (Screen.height * 179/768));
 		GUILayout.Label (consoleLog);
 		GUILayout.EndScrollView();
 
@@ -26,12 +30,12 @@ public class GUI_Terminal : MonoBehaviour {
 				SubmitCommand();
 			}
 		}
-		if(GUI.Button(new Rect(0, (Screen.height * 211/768), 60,(Screen.height * 40/768) ),"Submit"))
+		if(GUI.Button(new Rect(0, (Screen.height * 179/768), 60,(Screen.height * 40/768) ),"Submit"))
 		{
 			SubmitCommand();
 		}
 		GUI.SetNextControlName("textField");
-		input = GUI.TextField(new Rect(70,  (Screen.height * 211/768), Screen.width - 90,(Screen.height * 40/768)), input);
+		input = GUI.TextField(new Rect(70,  (Screen.height * 179/768), Screen.width - 100,(Screen.height * 40/768)), input);
 		GUILayout.EndArea();
 
 	}
