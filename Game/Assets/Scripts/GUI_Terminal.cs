8c1b7882c7c846b8e0a453cc6ec38dc3be888d24
Jonathan Petz <jpegthedev@gmail.com>
2014-09-25 16:47:34 -0500

    Updated GUI code
    Updated some of the base GUI code, bringing better functionality and
    other stuff.

--- Game/Assets/Scripts/GUI_Terminal.cs
+++ Game/Assets/Scripts/GUI_Terminal.cs
@@ -18,8 +18,8 @@ public class GUI_Terminal : MonoBehaviour {
 		GUI.DrawTexture(new Rect(0,  Screen.height - (Screen.height * 256/768), Screen.width, Screen.height * 256/768), clb, ScaleMode.StretchToFill, true, 10.0F);
 
 
-		GUILayout.BeginArea(new Rect(16, Screen.height -  (Screen.height * 240/768), Screen.width - 28, (Screen.height * 240/768)));
-		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 28), GUILayout.Height (Screen.height * 179/768));
+		GUILayout.BeginArea(new Rect(24, Screen.height -  (Screen.height * 240/768), Screen.width - 48, (Screen.height * 240/768)));
+		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width (Screen.width - 48), GUILayout.Height (Screen.height * 179/768));
 		GUILayout.Label (consoleLog);
 		GUILayout.EndScrollView();
 
@@ -35,7 +35,7 @@ public class GUI_Terminal : MonoBehaviour {
 			SubmitCommand();
 		}
 		GUI.SetNextControlName("textField");
-		input = GUI.TextField(new Rect(70,  (Screen.height * 179/768), Screen.width - 100,(Screen.height * 40/768)), input);
+		input = GUI.TextField(new Rect(70,  (Screen.height * 179/768), Screen.width - 120,(Screen.height * 40/768)), input);
 		GUILayout.EndArea();
 
 	}
@@ -43,10 +43,11 @@ public class GUI_Terminal : MonoBehaviour {
 	{
 
 		string output = Parser.Parse(input);
+		scrollPosition.y = Mathf.Infinity;
 		if(!input.Equals(""))
 		{
 			consoleLog =  consoleLog + input +"\n";
-			scrollPosition.y = Mathf.Infinity;
+
 		}
 
 		consoleLog = consoleLog + output + "\n" ;
