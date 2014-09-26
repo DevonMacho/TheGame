4addba49a97bbe3a6f3be78d0e8d08b621f824ab
Jonathan Petz <jpegthedev@gmail.com>
2014-09-26 08:54:26 -0500

    added some incomplete code for items and inventory
    ^ that

---
+++ Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {	
+	public static Dictionary<ItemData.Item, string> playerInventory;
+	void initPlayerInv () 
+	{
+		if (playerInventory == null)
+		{
+			playerInventory = new Dictionary<ItemData.Item,string >();
+		}
+		return;
+	}
+	void Update () 
+	{
+	
+	}
+}
---
+++ Game/Assets/Scripts/ItemData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemData : MonoBehaviour {
+
+	public struct Item
+	{
+		public int weight;
+		public string name;
+		public string description;
+	}
+}
