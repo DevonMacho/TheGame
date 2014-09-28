9fc81453d5787bdd01f753960d3aa0ad4e3d2ddc
Jonathan Petz <jpegthedev@gmail.com>
2014-09-28 17:31:02 -0500

    Go Command implemented
    created WorldData to hold locations and implemented a go command to
    switch between them

---
+++ Game/Assets/Scripts/WorldData.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldData : MonoBehaviour {
+
+	private static Dictionary<string, int> locationList;
+	public static int currentLoc = 0;
+	public static void initializeLocations ()
+	{
+		locationList = new Dictionary<string,int >();
+		locationList.Add("Location A", 0);
+		locationList.Add("Location B", 1);
+	}
+	//public static string getCurrent()
+	//{
+	//	return locationList[currentLoc];
+	//}
+	public static string Go(string newLoc)
+	{
+		if (newLoc.Equals ("location a")) {
+			currentLoc = 0;
+			return "You are now at "+ "Location A";
+
+		} else if (newLoc.Equals ("location b")) {
+			currentLoc = 1;
+			return "You are now at "+ "Location B";
+		} else {
+			return "there is no such location";
+		}
+
+	}
+
+	void Start () {
+	
+	}
+
+
+}
