cc3412671d989f8c851d52accd3ef706f5837b38
Jonathan Petz <JPEGtheDev@gmail.com>
2014-11-16 06:51:01 -0600

    I can now wrap arrays
    ^^ that, I'm going to bed. this is a proof of concept for wrapping int
    arrays. this could be helpful for shifting 2dtextures in unity.

---
+++ Scraps/WrapArray.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WrapArray : MonoBehaviour
+{
+
+		
+		
+		// Use this for initialization
+		void Start ()
+		{
+				int[] intBefore = {0,1,2,3,4,5,6,7};
+				string before = "[";
+				foreach (int a in intBefore) {
+						before = before + " " + a;
+				}
+				before = before + " ]";
+				Debug.Log ("Before:\t" + before);
+				
+				
+				
+				for (int i = -10; i < 10; i++) {
+						int[] intAfter = arrayShift (i, intBefore);
+						string after = "[";
+						foreach (int a in intAfter) {
+								after = after + " " + a;
+						}
+						after = after + " ]";
+						Debug.Log ("Input Value: " + i + "\t" + "Shift Value: " + i % intBefore.Length + "\t" + "After:\t" + after);
+				}
+				
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		int[] arrayShift (int value, int[] shift)
+		{
+				int v2 = value % shift.Length;
+				List<int> temp = new List<int> ();
+				if (v2 > 0) {
+				
+				
+						for (int i = v2; i > 0; i--) {
+								temp.Add (shift [shift.Length - i]);
+						}
+				
+						for (int i = 0; i < shift.Length; i++) {
+								if (i + v2 < shift.Length) {
+										temp.Add (shift [i]);
+								}
+								
+								
+								
+						}
+						return temp.ToArray ();
+						
+						
+				
+				} else if (v2 < 0) {
+						
+			
+						for (int i = -v2; i < shift.Length; i++) {
+								if (i + v2 < shift.Length) {
+										temp.Add (shift [i]);
+								}
+							
+						}
+						for (int i = 0; i < -v2; i++) {
+								temp.Add (shift [i]);
+				
+						}
+						return temp.ToArray ();
+				}
+				return shift;
+		}
+}
