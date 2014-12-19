1b34f51a7326ab7087b981215e884301a9d33e3f
Jonathan Petz <jpegthedev@gmail.com>
2014-12-19 14:52:16 -0500

    Worked on the main menu a bit
    ^ that mostly and I’m getting used to the new GUI system in Unity3d.

---
+++ GameRedux/Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void StartNewGame()
+	{
+		Debug.Log("Starting New Game");
+	}
+	public void LoadGame()
+	{
+		Debug.Log("Loading Game");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quitting Game");
+	}
+}
