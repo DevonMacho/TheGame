e119d96335c92ff627fe29128c4245544be25851
Jonathan Petz <jpegthedev@gmail.com>
2014-12-20 15:00:46 -0500

    Still working on the settings
    Still working on the settings GUI and I’m making a lot of progress with
    coding with the new UI system.

---
+++ GameRedux/Assets/Scripts/Options.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Options : MonoBehaviour {
+	public GameObject ResPanel;
+	public GameObject ResText;
+	public GameObject MSlider;
+	public GameObject ESlider;
+	public float MusicVolume;
+	public float EffectVolume;
+	float tempEV;
+	float tempMV;
+	// Use this for initialization
+	public void Awake ()
+	{
+		//load every time options is clicked
+		//load volume from file
+		//load resolution from file
+		Debug.Log( "Options is awake");
+		ResText.GetComponent<Text>().text = Screen.width + "x" + Screen.height;
+
+		//set Volume Sliders
+		//Debug.Log(Screen.width + "x" + Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+		public void ToggleRes()
+	{
+		ResPanel.SetActive(!ResPanel.activeSelf);
+	}
+	public void setTempEV(float tmp)
+	{
+		tempEV = tmp;
+	}
+	public void setTempMV(float tmp)
+	{
+		tempMV = tmp;
+	}
+}
