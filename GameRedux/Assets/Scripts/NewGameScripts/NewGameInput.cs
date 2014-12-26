1ce070477accbe5e3a6be3fa578d61e01d360578
Jonathan Petz <jpegthedev@gmail.com>
2014-12-26 00:08:15 -0500

    Got the UI the way that I want it (almost)
    I almost got the ui the way that I wanted it to be. One thing that is
    kind of frustrating is the new submit system. Some people online have
    some solutions but I thought that I should commit before I fuck
    anything up or change anything.

---
+++ GameRedux/Assets/Scripts/NewGameScripts/NewGameInput.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class NewGameInput : MonoBehaviour {
+	
+	public GameObject SubmitButton;
+	public GameObject InputField;
+	public GameObject InputText;
+	public GameObject OutputText;
+	public GameObject ScrollBar;
+	//bool _acceptInput;
+
+
+	void Start()
+	{
+		InputField.GetComponent<InputField>().onEndEdit.RemoveAllListeners();
+		InputField.GetComponent<InputField>().onEndEdit.AddListener(delegate 
+		{
+			//Submit();
+		});
+
+		SubmitButton.GetComponent<Button>().onClick.RemoveAllListeners();
+		SubmitButton.GetComponent<Button>().onClick.AddListener(delegate 
+		{
+			Submit();
+
+		});
+	}
+	public void Submit()
+	{
+		ScrollBar.GetComponent<Scrollbar>().value = 0.0000000000000f;
+		string input = InputText.GetComponent<Text>().text;
+		Debug.Log(input);
+		//send input text to the parser
+		//get response from parser and output it to OutputText
+	}
+	/*
+	public void AcceptInput(bool value)
+	{
+		_acceptInput = value;
+	}
+	*/
+}
